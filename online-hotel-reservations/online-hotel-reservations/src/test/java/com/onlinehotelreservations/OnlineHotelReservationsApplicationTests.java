aab0833c510eb327519fc5a8ee56b51438741dff
Trinh Le Cong <congtrjnh@gmail.com>
2020-05-24 16:44:18 +0700

    initial project

---
+++ online-hotel-reservations/online-hotel-reservations/src/test/java/com/onlinehotelreservations/OnlineHotelReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinehotelreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineHotelReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
